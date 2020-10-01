c58ae809dbd47a7171e15e0f131ba72951d7ea49
CodieKev <rohitprasad231@gmail.com>
2020-10-01 15:43:57 +0530

    Change_Name

---
+++ Custon_5_Feature_Mapping/optimal_params.py
@@ -0,0 +1,22 @@
+# # the write_and_run function writes the content in this cell into the file "optimal_params.py"
+
+### WRITE YOUR CODE BETWEEN THESE LINES - START
+    
+# import libraries that are used in the function below.
+import numpy as np
+    
+### WRITE YOUR CODE BETWEEN THESE LINES - END
+
+def return_optimal_params():
+    # STORE THE OPTIMAL PARAMETERS AS AN ARRAY IN THE VARIABLE optimal_parameters 
+    
+    optimal_parameters =[ 1.40259467, -0.09599713 ,-1.05596403 , 0.89133825 ,-1.2556299 ,  1.65823062,
+ -0.49055336 ,-0.3077759  , 2.67018913 , 0.98293535 , 0.63819836 , 1.29900352,
+ -0.36556585 , 0.14759602 , 1.96899803 ,-0.21064131 , 1.54638799 , 0.23352886,
+ -0.53818219 ,-0.8641724  , 0.12778359 , 1.2081973  ,-2.16621513 ,-0.52989942,
+ -0.72726445 , 0.28030148 , 1.6120112  , 0.55925266 ,-1.73603201 , 0.53441027,
+  0.49504603 , 0.10068782 ,-0.22465866 , 1.49910893 ,-0.28809495 , 0.68688422,
+  0.02926408 ,-0.10336585 , 0.92108278 ,-0.29384068]
+    
+    # STORE THE OPTIMAL PARAMETERS AS AN ARRAY IN THE VARIABLE optimal_parameters 
+    return np.array(optimal_parameters)
