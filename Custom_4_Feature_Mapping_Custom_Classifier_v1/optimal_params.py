f7d433dc6d2977bab4c392faa6f88a87ac2ef91b
CodieKev <rohitprasad231@gmail.com>
2020-10-01 17:05:25 +0530

    Editing

---
+++ Custom_4_Feature_Mapping_Custom_Classifier_v1/optimal_params.py
@@ -0,0 +1,22 @@
+# # the write_and_run function writes the content in this cell into the file "optimal_params.py"
+
+### WRITE YOUR CODE BETWEEN THESE LINES - START
+    
+# import libraries that are used in the function below.
+import numpy as np
+    
+### WRITE YOUR CODE BETWEEN THESE LINES - END
+
+def return_optimal_params():
+    # STORE THE OPTIMAL PARAMETERS AS AN ARRAY IN THE VARIABLE optimal_parameters 
+    
+    optimal_parameters =[ 3.63795705 , 1.19128865 ,-0.33132838 , 0.74721243 , 0.92831932 , 0.42240372,
+  0.17701117 , 1.80778497,  1.54819545 , 2.14835959 , 0.0773745 , -1.0679474,
+  1.10294989 ,-0.69464833,  1.17269839 ,-1.03707315 ,-0.64726175 ,-1.42869361,
+ -2.25336805 ,-1.54722281 ,-0.5098562 , -0.83212129 , 0.8265452  , 0.78973385,
+ -1.01799251, -0.18191186 , 1.05282282, -0.93642473 ,-0.60919723 ,-2.02198214,
+  0.25290384 ,-0.69921034  ,0.52687956, -0.26904783 , 0.01643734 , 0.17502405,
+  1.64029289, -0.43707053]
+    
+    # STORE THE OPTIMAL PARAMETERS AS AN ARRAY IN THE VARIABLE optimal_parameters 
+    return np.array(optimal_parameters)
